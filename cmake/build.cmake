e66910c223676dbc5e6e344a34630d5126d0bb17
vin manager <fan_jingli@126.com>
2022-06-12 11:16:44 +0800

    add cmake base

---
+++ cmake/build.cmake
@@ -0,0 +1,3 @@
+
+include(cmake/variable.cmake)
+include(cmake/function.cmake)
---
+++ cmake/variable.cmake
@@ -0,0 +1,13 @@
+
+# if(NOT EXISTS ${CMAKE_INSTALL_PREFIX})
+#     file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX})
+# endif()
+message(STATUS "CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
+
+include_directories(
+    ${PROJECT_SOURCE_DIR}/3rdparty/gtest/include/
+)
+
+link_directories(
+    ${PROJECT_SOURCE_DIR}/3rdparty/gtest/libs/
+)
